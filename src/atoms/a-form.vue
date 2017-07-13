4561ae56bdcb70cd04d744626d6f56eb6c4a3fc8
Przemysław Drążewski <pdrazewski@Przemysaws-MacBook-Air.local>
2017-07-13 22:52:58 +0200

    Refactor styles

---
+++ src/atoms/a-form.vue
@@ -0,0 +1,89 @@
+<template>
+	<div class="a-form">
+		<form :name="form.name" :method="form.method" :action="form.action">
+			<div v-for="(row, index) in rows" class="a-form_row">
+				<div class="a-form_label">
+					<label class="a-form_label" for="name">
+						<label>{{row.label}}</label>
+						<span v-if="row.required" class="a-form_sup">*</span>
+					</label>
+				</div>
+				<div class="a-form_field">
+					<template v-if="row.type === 'textarea'">
+						<textarea></textarea>
+					</template>
+					<template v-else>
+						<input :type="row.type" class="a-form_text" :class="row.customClass" v-model="row.model"/>
+						<template v-if="row.data">
+							{{row.data}}
+						</template>
+					</template>
+				</div>
+			</div>
+			<template v-if="submit.type === 'button'">
+				<button type="submit">{{submit.text}}</button>
+			</template>
+		</form>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'atom-form',
+		data () {
+			return {
+				form: {
+					name: 'form',
+					method: 'post',
+					action: '/'
+				},
+				rows: [{
+					label: 'Imię',
+					required: true,
+					type: 'text',
+					customClass: false,
+					data: false
+				}, {
+					label: 'Nazwisko',
+					required: true,
+					type: 'text',
+					customClass: false,
+					data: false
+				}, {
+					label: 'Data urodzenia',
+					required: true,
+					type: 'date',
+					customClass: 'js-pikaday',
+					data: false
+				}, {
+					label: 'Data urodzenia',
+					required: true,
+					type: 'radio',
+					customClass: 'js-pikaday',
+					data: false
+				}, {
+					label: 'Data urodzenia',
+					required: true,
+					type: 'select',
+					customClass: 'js-pikaday',
+					data: false
+				}, {
+					label: 'Data urodzenia',
+					required: true,
+					type: 'checkbox',
+					customClass: 'js-pikaday',
+					data: false
+				}, {
+					label: 'Data urodzenia',
+					required: true,
+					type: 'textarea',
+					customClass: 'js-pikaday',
+					data: false
+				}],
+				submit: {
+					type: 'button',
+					text: 'Wyślij'
+				}
+			}
+		}
+	}
+</script>
--- src/components/widgetEditor.vue
+++ src/components/widgetEditor.vue
@@ -1,53 +1,128 @@
 <template>
 	<div >
 		<div>
-			<form class="a-form" @submit.prevent="compile">
+			<form class="a-form"@submit.prevent="save">
 				<div class="c-accordion clearfix">
 					<div class="c-accordion_item">
 						<input type="radio" id="static-tab-1" name="static-items" checked="checked">
-						<label for="static-tab-1">Dane podstawowe</label>
+						<label class="c-accordion_label" for="static-tab-1">Dane podstawowe</label>
 						<div class="c-accordion_content">
 							<div class="c-accordion_contentRaw">
-							   	Nazwa: Accordion <br>
-								Autor: pd<br>
-								Typ: default|mail|allegro|lp<br>
-								Kategoria: default|oferta|<br>
-								Publiczny/prywatny: ok<br>
-								Opis: this is sparta<br>
-								Miniaturka: this is mini turkey<br>
-								Srodowisko: Samsung|Core <br>
+								<div class="a-form_row">
+									<div class="a-form_label">
+										<label class="a-form_label" for="name">
+											<label>Nazwa</label>
+										</label>
+									</div>
+									<div class="a-form_field">
+										 <input type="text" class="a-form_text" v-model="basic.name" />
+									</div>
+								</div>
+								<div class="a-form_row">
+									<div class="a-form_label">
+										<label class="a-form_label" for="name">
+											<label>Autor</label>
+										</label>
+									</div>
+									<div class="a-form_field">
+										 <input type="text" class="a-form_text" v-model="basic.author" />
+									</div>
+								</div>
+								<div class="a-form_row">
+									<div class="a-form_label">
+										<label class="a-form_label" for="name">
+											<label>Typ</label>
+										</label>
+									</div>
+									<div class="a-form_field">
+										 <input type="text" class="a-form_text" v-model="basic.type" />
+									</div>
+								</div>
+								<div class="a-form_row">
+									<div class="a-form_label">
+										<label class="a-form_label" for="name">
+											<label>Kategoria</label>
+										</label>
+									</div>
+									<div class="a-form_field">
+										 <input type="text" class="a-form_text" v-model="basic.category" />
+									</div>
+								</div>
+								<div class="a-form_row">
+									<div class="a-form_label">
+										<label class="a-form_label" for="name">
+											<label>Publiczny/prywatny:</label>
+										</label>
+									</div>
+									<div class="a-form_field">
+										 <input type="text" class="a-form_text" v-model="basic.access" />
+									</div>
+								</div>
+								<div class="a-form_row">
+									<div class="a-form_label">
+										<label class="a-form_label" for="name">
+											<label>Opis:</label>
+										</label>
+									</div>
+									<div class="a-form_field">
+										 <textarea rows="12" type="text" class="a-form_text" v-model="basic.description"></textarea>
+									</div>
+								</div>
+								<div class="a-form_row">
+									<div class="a-form_label">
+										<label class="a-form_label" for="name">
+											<label>Miniaturka:</label>
+										</label>
+									</div>
+									<div class="a-form_field">
+										 <input type="text" class="a-form_text" v-model="basic.thumbnail" />
+									</div>
+								</div>
+								<div class="a-form_row">
+									<div class="a-form_label">
+										<label class="a-form_label" for="name">
+											<label>Srodowisko:</label>
+										</label>
+									</div>
+									<div class="a-form_field">
+										 <input type="text" class="a-form_text" v-model="basic.environment" />
+									</div>
+								</div>
 							</div> 	
 						</div>
 					</div>
 					<div class="c-accordion_item">
 						<input type="radio" id="static-tab-2" name="static-items">
-						<label for="static-tab-2">Szablon (template)</label>
+						<label class="c-accordion_label" for="static-tab-2">Szablon (template)</label>
 						<div class="c-accordion_content">
-							<div class="c-accordion_contentRaw">
-							   	
-								<textarea v-model="template" class="a-form_field" type="text" @input="validateTemplate();" style="height: 500px; line-height: 20px;">
-								test
-								</textarea>
-								<div v-for="(varName, index) in vars">
-				<p>Ustawienia pola: {{varName.data}} ({{varName.items}} )</p>
-				<label>Nazwa wyświetlana: <input type="text"></label>
-				Typ pola <select>
-				<option>String</option>
-				<option>Number</option>
-				<option>Filemanager (img)</option>
-				<option>Tablica elementów</option>
-				</select>
-				</p>
-			</div>
+							<div class="c-accordion_contentRaw">	
+								<div class="a-form_field">					   	
+								<textarea v-model="template" class="a-form_text" rows="20" type="text" @input="validateTemplate();"></textarea>
+								</div>
+								<div class="a-form_legend">
+									<div v-for="(varName, index) in vars">
+										<p>Ustawienia pola: {{varName.data}} ({{varName.items}} )</p>
+										<label>Nazwa wyświetlana: <input type="text"></label>
+										<p style="margin-top:10px">
+											Typ pola 
+											<select>
+											<option>String</option>
+											<option>Number</option>
+											<option>Filemanager (img)</option>
+											<option>Tablica elementów</option>
+											</select>
+										</p>
+									</div>
+								</div>
 							</div> 	
 						</div>
 					</div>
 					<div class="c-accordion_item">
 						<input type="radio" id="static-tab-3" name="static-items" >
-						<label for="static-tab-3">Style (scss)</label>
+						<label class="c-accordion_label" for="static-tab-3">Style (scss)</label>
 						<div class="c-accordion_content">
 							<div class="c-accordion_contentRaw">
-								<textarea v-model="styles" class="a-form_field" type="text" style="height: 500px; line-height: 20px;">
+								<textarea v-model="styles" class="a-form_field" type="text" >
 								test
 								</textarea>
 							</div> 	
@@ -55,7 +130,7 @@
 					</div>
 					<div class="c-accordion_item">
 						<input type="radio" id="static-tab-4" name="static-items">
-						<label for="static-tab-4">Javascript</label>
+						<label class="c-accordion_label" for="static-tab-4">Javascript</label>
 						<div class="c-accordion_content">
 							<div class="c-accordion_contentRaw">
 								Dodaj bibiloteki (head, stopka)
@@ -67,7 +142,7 @@
 					</div>
 					<div class="c-accordion_item">
 						<input type="radio" id="static-tab-5" name="static-items">
-						<label for="static-tab-5">Podgląd konfiguracji (testowe dane)</label>
+						<label class="c-accordion_label" for="static-tab-5">Podgląd konfiguracji (testowe dane)</label>
 						<div class="c-accordion_content">
 							<div class="c-accordion_contentRaw">
 								<div v-html="rawHtml"></div>
@@ -76,7 +151,7 @@
 					</div>
 					<div class="c-accordion_item">
 						<input type="radio" id="static-tab-6" name="static-items">
-						<label for="static-tab-6">Podgląd</label>
+						<label class="c-accordion_label" for="static-tab-6">Podgląd</label>
 						<div class="c-accordion_content">
 							<div class="c-accordion_contentRaw">
 								<div v-html="rawHtml"></div>
@@ -107,29 +182,24 @@
 		name: 'widget-editor',
 		data() {
 			return {
-				placeholder: "My amazing widget",
-				name: "",
 				key: "",
+				basic: {
+					name: "",
+					author: "",
+					type: "",
+					category: "",
+					access: "",
+					description: "",
+					thumbnail: "",
+					environment: ""
+				},
 				template: "",
 				styles: "",
 				vars: [],
 				data: {},
 				saved: '',
 				loops: [],
-				rawHtml: '',
-				tabs: [{
-					name: "Dane podstawowe",
-					content: this.placeholder
-				},{
-					name: "Szablon (template)",
-					content: "AAAAA BBB CCC"
-				},{
-					name: "Style (css)",
-					content: "AAAAA BBB CCC"
-				},{
-					name: "Javascript",
-					content: "AAAAA BBB CCC"
-				}]
+				rawHtml: ''
 			}
 		},
 		computed: {
@@ -166,16 +236,15 @@
 					})
 				}
 			},
-			compile() {
+			save() {
 				this.rawHtml = nunjucks.renderString(this.template, this.data);
 				let data = {
 					db: 'widgets',
 					key: this.key,
 					data: {
-						author: this.$store.state.user.id,
+						basic: this.basic,
 						template: this.template,
 						style: this.styles,
-						name: this.name,
 						key: this.key,
 						vars: this.vars
 					}
